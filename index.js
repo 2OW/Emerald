b54a9fcb1729be4c7bc2ee0231c4d3ebb29fd352
FactoDoesStuff <factoman4@gmail.com>
2017-06-15 00:59:42 -0400

    Create index.js

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ client.on('ready', () => {
     console.log('Ready to work, boss!')
 });
 
+client.on('message', message => {
+    if(message.content === '!ping'){
+        message.channel.send('I forget what it is, ' + message.author.username);
+    }
+});
+
 client.login('NeverWillYouFindThisOut');
