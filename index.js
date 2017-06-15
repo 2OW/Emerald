0a43ba8a03776c8e01ffe9ff9e05e763aa689a3a
FactoDoesStuff <factoman4@gmail.com>
2017-06-15 00:23:09 -0400

    Create index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('Ready to work, boss!')
+});
+
+client.login('NeverWillYouFindThisOut');
